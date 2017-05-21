27c750b19a95d5bff733e3604cb2aff7528ba7b5
Harshita Jhavar <harshitajhavar@gmail.com>
2017-05-21 15:06:10 +0200

    Language Modelling based on bigram frequency

--- question1.py
+++ question1.py
@@ -1,89 +1,69 @@
-#Solution of question1 of exercise 1
+#Assignment 2
+#Solution to Question 1
 
-#Importing re package to use regular expression as a tokenizer
-import re 
-#Importing matplotlib for plotting
-import matplotlib.pyplot as plt 
-#Importing itemgetter for generating the frequency table
-from operator import itemgetter 
+from nltk.corpus import brown #Importing brown corpus from nltk
+import matplotlib.pyplot as plt #For plotting
+import math #For taking log to calculate expected value
 
-#Reading all three text files
-english_file = open('./data/english.txt', 'r')
-french_file = open('./data/french.txt', 'r')
-german_file = open('./data/german.txt', 'r')
+def find_followUp_word_given_word(word):
+	'''Making a list of all the words following the given word'''
+	next_word = []
+	for i in range(len(text)-1):
+		if(text[i] == word):
+			next_word.append((text[i+1]))
+	return next_word
 
-#Tokenizing the three texts using regular expression.
-#The regular expression reads every word whether beginning with a capital or a small letter and the follow up alphabets are marked with question mark to allow words of length 1 also like 'I', 'a' etc.
-english_tokens = re.findall(r'(\b[A-Za-z]+[a-z]?\b)', english_file.read())
-french_tokens = re.findall(r'(\b[A-Za-z]+[a-z]?\b)', french_file.read())
-german_tokens = re.findall(r'(\b[A-Za-z]+[a-z]?\b)', german_file.read())
+def calculate_relative_frequency(next_word):	
+	'''Storing (word following the given word, number of times it follows the given word, relative frequency).  Basically, calculating the conditional probability distribution'''
+	relative_frequency = []
+	for i in range(len(next_word)-1):
+		relative_frequency.append((next_word[i], next_word.count(next_word[i]), (next_word.count(next_word[i]) / float(len(next_word)))))
+	#Removing duplicate entries from the list of relative frequency using the set function.
+	relative_frequency = list(set(relative_frequency))
+	return(relative_frequency)
 
-#Initializing three empty dictionaries so as to hold the frequency of each different word in the texts
-english_frequency = {}
-french_frequency = {}
-german_frequency = {}
+def find_top_20_tokens(relative_frequency):	
+	'''Finding the top 20 words'''
+	top_20 = (sorted(relative_frequency, key=lambda relative_frequency: relative_frequency[1], reverse=True))[:20]   # sort by count
+	return(top_20)
 
-#Calculating the frequency for each word in English
-for word in english_tokens:
-    english_count = english_frequency.get(word,0)
-    english_frequency[word] = english_count + 1
-    
-#Calculating the frequency for each word in French
-for word in french_tokens:
-    french_count = french_frequency.get(word,0)
-    french_frequency[word] = french_count + 1
-    
-#Calculating the frequency for each word in German   
-for word in german_tokens:
-    german_count = german_frequency.get(word,0)
-    german_frequency[word] = german_count + 1
+def plot_frequency_distribution(top_20, word):	
+	'''Plotting the required graph'''
+	x = []
+	y = []
+	for i in range(len(top_20)):
+		x.append(top_20[i][0])
+		y.append(top_20[i][1])
+	plt.plot(y)
+	plt.xlabel("Top 20 tokens following the word - " +word)
+	plt.ylabel("Frequency Distribution ")
+	plt.title("Frequency Distribution for top 20-tokens following the word - "+word)
+	plt.show()
 
-#Sorting the frequencies in reverse order so as to assign a rank to the words in all three different texts and generating a lists in form of pair (word, frequency) in ascending order of rank.
-english_ranks = []
-german_ranks = []
-french_ranks = []
-english_table = []
-german_table = []
-french_table = []
+def calculate_expected_value(relative_frequency):
+	'''Computing the expected value for the distributions'''
+	expected_value = 0
+	for i in range(len(relative_frequency)):
+		expected_value = expected_value + ( relative_frequency[i][1] * -1 * math.log(relative_frequency[i][2], 2 ) )#log to the base 2
+	return(str(expected_value))
 
-#For English text
-for key, value in reversed(sorted(english_frequency.items(), key = itemgetter(1))):
-    english_table.append([key, value])
-    english_ranks.append(value)
-    
-#For French text
-for key, value in reversed(sorted(french_frequency.items(), key = itemgetter(1))):
-    french_table.append([key, value])
-    french_ranks.append(value)
-    
-#For German text
-for key, value in reversed(sorted(german_frequency.items(), key = itemgetter(1))):
-    german_table.append([key, value])
-    german_ranks.append(value)
 
-#Printing the generated word frequency table as mentioned in the question.
-#For English
-print("Word   ::  Frequency")
-for item in english_table:
-	print(item[0],"  ::  ",item[1]) 
-#For French
-print("Word   ::  Frequency")
-for item in french_table:
-	print(item[0],"  ::  ",item[1]) 
-#For German	    
-print("Word   ::  Frequency")
-for item in german_table:
-	print(item[0],"  ::  ",item[1]) 
-	  
-#Plotting the frequencies on a logarithmic scale
-plt.xscale('log')
-plt.yscale('log')
-e, = plt.plot(english_ranks, color = 'red', label='English Text Word Frequency')
-f, = plt.plot(french_ranks, color = 'blue', label='French Text Word Frequency')
-g, = plt.plot(german_ranks, color = 'green', label='German Text Word Frequency')
-plt.xlabel('Rank')
-plt.ylabel('Frequency')
-plt.title("Zipf's Law on Othello by Shakespeare")
-plt.legend(handles=[e,f,g])
-plt.show()        
+#Tokenizing and lowercasing each token
+text = brown.words()
+text = [x.lower() for x in text]
+
+#For the word - "in"
+next_word_in = find_followUp_word_given_word("in") 
+relative_frequency_in = calculate_relative_frequency(next_word_in)
+top_20_in = find_top_20_tokens(relative_frequency_in)
+plot_frequency_distribution(top_20_in, "in")
+print("The expected value for 'in' is " +calculate_expected_value(relative_frequency_in))
+
+
+#For the word - "the"
+next_word_the = find_followUp_word_given_word("the")
+relative_frequency_the = calculate_relative_frequency(next_word_the)
+top_20_the = find_top_20_tokens(relative_frequency_the)
+plot_frequency_distribution(top_20_the, "the")
+print("The expected value for 'the' is " +calculate_expected_value(relative_frequency_the))
 
